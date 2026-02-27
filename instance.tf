df46e6ddd937b6aeafe6613954aab705966b1caa
Mangesh Bonde <connect.mangeshbonde@gmail.com>
2026-02-27 22:09:41 +0530

    Update AWS instance configuration and key name

--- instance.tf
+++ instance.tf
@@ -2,18 +2,14 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "server-mumbai"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGH7HjVlWdY3TTXoc0o+qwWezI/6qZn+FC5NbXlADd8O 91937@Mangesh"
-}
 
 resource "aws_instance" "example" {
   ami           = "ami-051a31ab2f4d498f5"
   instance_type = "t3.micro"
-  key_name      = aws_key_pair.deployer.key_name
+  key_name      = "mumbai-key"
   count         = 3
 
   tags = {
-    Name = "instance-mumbai"
+    Name = "web-server-${count.index+1}"
   }
 }
