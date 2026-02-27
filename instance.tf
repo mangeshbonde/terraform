3e9b86f014a46ac8feb33145b415ebd27295f1b2
Mangesh Bonde <connect.mangeshbonde@gmail.com>
2026-02-27 11:25:40 +0530

    New file added

---
+++ instance.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "ap-south-1"
+    access_key = "AKIAS2FJUFVC2NJ3FIN3"
+    secret_key = "SoYc8B+vHL30iL27jsEzwuK+prBSNKYWyYVIeTXo"
+  
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-051a31ab2f4d498f5"
+    instance_type = "t3.micro"
+    key_name      = aws_key_pair.deployer.key_name
+    count         = 3
+    tags = {
+        Name = "instance-mumbai"
+}
+resource "aws_key_pair" "deployer" {
+  key_name   = "server-mumbai"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGH7HjVlWdY3TTXoc0o+qwWezI/6qZn+FC5NbXlADd8O 91937@Mangesh
+"
+}
