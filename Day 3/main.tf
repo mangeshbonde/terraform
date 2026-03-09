206bd0f0adb16e28041364b8bff87a87907cad59
Mangesh Bonde <connect.mangeshbonde@gmail.com>
2026-03-09 21:15:23 +0530

    All

--- Day 3/main.tf
+++ Day 3/main.tf
@@ -12,10 +12,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami           = var.ami_id
+  ami           = var.ami_id   
   instance_type = var.instance-type
   key_name      = var.key-name
-  vpc_security_group_ids = ["aws_security_group.my-sg-grp.id"]   #Here we are using the id of the security group that we imported to the terraform state.
+  vpc_security_group_ids = [aws_security_group.my-sg-grp.id]   #Here we are using the id of the security group that we imported to the terraform state.
              
   tags = {
     Name = "EC2-Instance"
